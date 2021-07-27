8416ce0dd80d604a9f794411037ef2587082d8fe
DirtyWhiskers <quinngairson@gmail.com>
2021-07-26 23:52:08 -0400

    Added Functions

--- game.js
+++ game.js
@@ -9,9 +9,9 @@ kaboom({
 });
 
 const MOVE_SPEED = 120;
-const JUMP_FORCE = 400;
+const JUMPHEIGHT = 400;
 let isJumping = true;
-const CURRENT_JUMP_FORCE = JUMP_FORCE;
+let CURRENT_JUMP = JUMPHEIGHT;
 const FALL_DEATH = 400;
 
 loadRoot('https://i.imgur.com/');
@@ -27,46 +27,48 @@ loadSprite('marioLeft', 'vujGU6O.png'); //  Mario left
 loadSprite('mushroom', '0wMd92p.png');
 loadSprite('surprise', 'gesQ1KP.png');
 loadSprite('unboxed', 'bdrLpi6.png');
-loadSprite('pipe-top-left', 'ReTPiWY.png');
-loadSprite('pipe-top-right', 'hj2GK4n.png');
-loadSprite('pipe-bottom-left', 'c1cYSbt.png');
-loadSprite('pipe-bottom-right', 'nqQ79eI.png');
+loadSprite('pipeTopLeft', 'ReTPiWY.png'); // top left pipe
+loadSprite('pipeTopRight', 'hj2GK4n.png'); // top right pipe
+loadSprite('pipeBottomLeft', 'c1cYSbt.png'); // bottom left pipe
+loadSprite('pipeBottomRight', 'nqQ79eI.png'); // bottom right pipe
 
-loadSprite('blue-block', 'fVscIbn.png');
-loadSprite('blue-brick', '3e5YRQd.png');
-loadSprite('blue-steel', 'gqVoI2b.png');
-loadSprite('blue-evil-shroom', 'SvV4ueD.png');
-loadSprite('blue-surprise', 'RMqCc1G.png');
+loadSprite('blueBlock', 'fVscIbn.png');
+loadSprite('blueBrick', '3e5YRQd.png');
+loadSprite('blueSteel', 'gqVoI2b.png');
+loadSprite('blueEvilShroom', 'SvV4ueD.png');
+loadSprite('blueSurprise', 'RMqCc1G.png');
 
 
-scene('game', ({level, score}) => {
+scene('game', ({ level, score }) => {
   layers(['bg', 'obj', 'ui'], 'obj');
 
   const maps = [
     [
-      '        c                                      c                   ',
-      '                          d                                          ',
-      '            %                                                    ',
-      '                                                                 ',
-      '                                                                 ',
-      '     %    =*=%=                                  -+                ',
-      '                                                 ()                ',
-      '                            -+        -+         ()                   ',
-      '                    ^   ^   ()   ^    ()         ()                    ',
-      '================================================================   ===========',
-      '================================================================   ===========',
+      '                      c                                                                                                                            ',
+      '                                          d                            c                                                                           ',
+      '         c                       d c                         c                                                                                     ',
+      '                         %                                                         ========     ==%               *            ===    =%%=         ',
+      '                                                                                                                                                   ',
+      '                                                                                                                                                   ',
+      '                  %    =*=%=                                                =*=                   =      ==    %  %  %     =           ==          ',
+      '                                                                                                                                                   ',
+      '                                  -+       -+     -+        -+                                                                                     ',
+      '                            ^     ()       () ^   ()   ^^   ()                                                                                     ',
+      '=======================================================================  ==============      ===============================================       ',
+      '=======================================================================  ==============      ===============================================       ',
     ],
     [
-      '£                                       £',
-      '£                                       £',
-      '£                                       £',
-      '£                                       £',
-      '£                                       £',
-      '£        @@@@@@              x x        £',
-      '£                          x x x        £',
-      '£                        x x x x  x   -+£',
-      '£               z   z  x x x x x  x   ()£',
-      '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',
+      '£    !!!!!!!    £',
+      '£               £',
+      '£               £',
+      '£     $$$$$     £',
+      '£    $$$$$$$    £',
+      '£    $$$$$$$    £',
+      '£    £££££££    £',
+      '£    £££££££  -+£',
+      '£    £££££££  ()£',
+      '!!!!!!!!!!!!!!!!!',
+      '!!!!!!!!!!!!!!!!!',
     ],
   ];
 
@@ -78,17 +80,17 @@ scene('game', ({level, score}) => {
     '%': [sprite('surprise'), solid(), 'coin-surprise'],
     '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
     '}': [sprite('unboxed'), solid()],
-    '(': [sprite('pipe-bottom-left'), solid(), scale(0.5), 'pipe-left'],
-    ')': [sprite('pipe-bottom-right'), solid(), scale(0.5), 'pipe-right'],
-    '-': [sprite('pipe-top-left'), solid(), scale(0.5), 'pipe'],
-    '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
-    '^': [sprite('evil-shroom'), {dir: -1}, 'dangerous'],
+    '(': [sprite('pipeBottomLeft'), solid(), scale(0.5), 'pipeLeft'],
+    ')': [sprite('pipeBottomRight'), solid(), scale(0.5), 'pipeRight'],
+    '-': [sprite('pipeTopLeft'), solid(), scale(0.5), 'pipe'],
+    '+': [sprite('pipeTopRight'), solid(), scale(0.5), 'pipe'],
+    '^': [sprite('evil-shroom'), { dir: -1 }, 'dangerous'],
     '#': [sprite('mushroom'), solid(), 'mushroom', body()],
-    '!': [sprite('blue-block'), solid(), scale(0.5)],
-    '£': [sprite('blue-brick'), solid(), scale(0.5)],
-    'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
-    '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
-    'x': [sprite('blue-steel'), solid(), scale(0.5)],
+    '!': [sprite('blueBlock'), solid(), scale(0.5)],
+    '£': [sprite('blueBrick'), solid(), scale(0.5)],
+    'z': [sprite('blueEvilShroom'), solid(), scale(0.5), 'dangerous'],
+    '@': [sprite('blueSurprise'), solid(), scale(0.5), 'coinSurprise'],
+    'x': [sprite('blueSteel'), solid(), scale(0.5)],
     'c': [sprite('cloud'), scale(0.08)],
     'd': [sprite('doubleCloud'), scale(0.1)],
 
@@ -106,6 +108,25 @@ scene('game', ({level, score}) => {
   ]);
 
   add([text('level ' + parseInt(level + 1)), pos(40, 6)]);
+
+  /**
+*@return {boolean}
+*/
+  function drinkMePotion() { // Character grow or shrink
+    return {
+      shrink() { // Character shrink
+        this.scale = vec2(1);
+        CURRENT_JUMP = JUMPFORCE;
+        return isBig = false;
+      },
+      growBig() { // Character grow
+        CURRENT_JUMP = BIG_JUMP_FORCE;
+        // player.changeSprite('evil-shroom')
+        this.scale = vec2(1.1);
+        return isBig = true;
+      },
+    };
+  }
 
   const player = add([
     sprite('mario'), solid(),
@@ -126,26 +147,34 @@ scene('game', ({level, score}) => {
     body();
   });
 
+  collides('dangerous', 'pipeRight', (d) => { //  Bournces between pipes
+    d.dir = -d.dir;
+  });
+
+  collides('dangerous', 'pipeLeft', (d) => { // Bounces between pipes
+    d.dir = -d.dir;
+  });
+
+  // FireBall function here.......................
+
+  let isBig = false;
   player.collides('dangerous', (d) => {
     if (isJumping) {
       destroy(d);
-    } else {
-      go('lose', {score: scoreLabel.value});
+    } else if (isBig) { //  If character big shrink
+      camShake(1);
+      wait(0.1, () => {
+        player.smallify();
+      });
+    } else { // If character is shrink kill
+      go('lose', { score: scoreLabel.value });
     }
-  });
-
-  collides('dangerous', 'pipe-right', (d) => {
-    d.dir = -d.dir;
-  });
-
-  collides('dangerous', 'pipe-left', (d) => {
-    d.dir = -d.dir;
   });
 
   player.action(() => {
     camPos(player.pos);
     if (player.pos.y >= FALL_DEATH) {
-      go('lose', {score: scoreLabel.value});
+      go('lose', { score: scoreLabel.value });
     }
   });
 
@@ -181,13 +210,13 @@ scene('game', ({level, score}) => {
   keyPress('space', () => {
     if (player.grounded()) {
       isJumping = true;
-      player.jump(CURRENT_JUMP_FORCE);
+      player.jump(CURRENT_JUMP);
     }
   });
 });
 
-scene('lose', ({score}) => {
+scene('lose', ({ score }) => {
   add([text(score, 32), origin('center'), pos(width() / 2, height() / 2)]);
 });
 
-start('game', {level: 0, score: 0});
+start('game', { level: 0, score: 0 });
